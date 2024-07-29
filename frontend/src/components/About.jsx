935856572ce0cb1c413edaf5a17a487754e493a5
Rafikhan15 <mdrafi.khan0089@gmail.com>
2024-07-29 10:20:55 +0600

    first commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we're serious about is food.</p>
+            </div>
+            <p className="mid">
+            Food is one of the basic necessities of life. 
+            Food contains nutrients—substances essential for the growth, repair, 
+            and maintenance of body tissues and for the regulation of vital processes. 
+            Nutrients provide the energy our bodies need to function.
+            </p>
+            <Link to={"/"}>
+              Explore Menu{" "}
+              <span>
+                <HiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">VILLAGE DINE</div>
+          <div className="right">
+            <p>12/1 Gulshan-2-Dhaka, Bangladesh</p>
+            <p>Open: 07:00 AM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By MdRafiKhan</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By MdRafiKhan.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
